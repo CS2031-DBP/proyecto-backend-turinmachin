80616a685a59ce555c2577f997e2c99ff2ce6929
matiasanaya <matias.anaya@utec.edu.pe>
2025-05-25 15:15:13 -0500

    feat:add bootstrap configuration

---
+++ src/main/java/com/turinmachin/unilife/configuration/BootstrapConfiguration.java
@@ -0,0 +1,47 @@
+package com.turinmachin.unilife.configuration;
+
+import com.turinmachin.unilife.user.domain.Role;
+import com.turinmachin.unilife.user.domain.User;
+import com.turinmachin.unilife.user.domain.UserService;
+import com.turinmachin.unilife.user.dto.RegisterUserDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BootstrapConfiguration {
+
+    private Logger logger = LoggerFactory.getLogger(BootstrapConfiguration.class);
+
+    @Value("${bootstrap.admin.email}")
+    private String adminEmail;
+
+    @Value("${bootstrap.admin.username}")
+    private String adminUsername;
+
+    @Value("${bootstrap.admin.password}")
+    private String adminPassword;
+
+    @Bean
+    public CommandLineRunner adminBootstrapper(UserService userService) {
+        return args -> {
+            if (userService.userExistsByRole(Role.ADMIN)) {
+                return;
+            }
+
+            logger.info("No admin users found. Creating default admin user");
+
+            RegisterUserDto dto = new RegisterUserDto();
+            dto.setEmail(adminEmail);
+            dto.setUsername(adminUsername);
+            dto.setPassword(adminPassword);
+            User admin = userService.createUser(dto);
+            userService.updateUserRole(admin, Role.ADMIN);
+            userService.verifyUser(admin);
+        };
+    }
+
+}
