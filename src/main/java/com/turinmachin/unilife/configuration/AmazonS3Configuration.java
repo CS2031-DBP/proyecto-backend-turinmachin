2fa268c6e34eed6f38df8c4bad24f61544fdd14c
José Daniel Grayson <josedanielgrayson@proton.me>
2025-05-24 23:38:22 -0500

    feat(config): add S3 configuration

---
+++ src/main/java/com/turinmachin/unilife/configuration/AmazonS3Configuration.java
@@ -0,0 +1,37 @@
+package com.turinmachin.unilife.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AmazonS3Configuration {
+
+    @Value("${amazon-s3.access-key}")
+    private String accessKey;
+
+    @Value("${amazon-s3.secret-key}")
+    private String secretKey;
+
+    @Value("${amazon-s3.session-token}")
+    private String sessionToken;
+
+    @Value("${amazon-s3.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        var credentials = new BasicSessionCredentials(accessKey, secretKey, sessionToken);
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+
+}
