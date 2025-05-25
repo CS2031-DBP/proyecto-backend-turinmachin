bc3dad5ba773d0ef5c6e39a0ebd3f2eaa317de24
Martin59lilo <bonillasarmientomartin@gmail.com>
2025-05-24 23:43:13 -0500

    feat: add methods to add a post and endpoints get all posts and get post by id

--- src/main/java/com/turinmachin/unilife/post/application/PostController.java
+++ src/main/java/com/turinmachin/unilife/post/application/PostController.java
@@ -1,30 +1,18 @@
 package com.turinmachin.unilife.post.application;
 
-import com.turinmachin.unilife.comment.domain.Comment;
 import com.turinmachin.unilife.comment.domain.CommentService;
-import com.turinmachin.unilife.comment.dto.CreateCommentDto;
-import com.turinmachin.unilife.comment.dto.UpdateCommentDto;
-import com.turinmachin.unilife.comment.exception.CommentNotFoundException;
 import com.turinmachin.unilife.common.domain.ListMapper;
-import com.turinmachin.unilife.common.exception.ForbiddenException;
-import com.turinmachin.unilife.common.exception.NotFoundException;
 import com.turinmachin.unilife.post.domain.Post;
 import com.turinmachin.unilife.post.domain.PostService;
-import com.turinmachin.unilife.post.domain.PostVote;
-import com.turinmachin.unilife.post.domain.VoteType;
-import com.turinmachin.unilife.user.domain.Role;
-import com.turinmachin.unilife.user.domain.User;
+import com.turinmachin.unilife.post.dto.PostResponseDto;
+import com.turinmachin.unilife.post.exception.PostNotFoundException;
+import com.turinmachin.unilife.post.infrastructure.PostSpecifications;
 import com.turinmachin.unilife.user.domain.UserService;
-import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -46,5 +34,25 @@ public class PostController {
 
     @Autowired
     private ListMapper listMapper;
+
+    @GetMapping
+    public List<PostResponseDto> getAllPosts(
+            @RequestParam(required = false) UUID universityId,
+            @RequestParam(required = false) List<String> tags,
+            @RequestParam(required = false) UUID authorId) {
+        Specification<Post> spec = Specification
+                .where(PostSpecifications.hasUniversityId(universityId))
+                .and(PostSpecifications.hasAuthorId(authorId))
+                .and(PostSpecifications.hasTags(tags));
+
+        List<Post> posts = postService.getPostsWithSpec(spec);
+        return listMapper.map(posts, PostResponseDto.class).toList();
+    }
+
+    @GetMapping("/{id}")
+    public PostResponseDto getPost(@PathVariable UUID id) {
+        Post post = postService.getPostById(id).orElseThrow(PostNotFoundException::new);
+        return modelMapper.map(post, PostResponseDto.class);
+    }
 
 }
--- src/main/java/com/turinmachin/unilife/post/domain/PostService.java
+++ src/main/java/com/turinmachin/unilife/post/domain/PostService.java
@@ -6,7 +6,13 @@ import com.turinmachin.unilife.post.infrastructure.PostVoteRepository;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
@@ -24,6 +30,18 @@ public class PostService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll(Sort.by("createdAt").descending());
+    }
+
+    public List<Post> getPostsWithSpec(Specification<Post> spec) {
+        return postRepository.findAll(spec, Sort.by("createdAt").descending());
+    }
+
+    public Optional<Post> getPostById(UUID id) {
+        return postRepository.findById(id);
+    }
 
 
 }
