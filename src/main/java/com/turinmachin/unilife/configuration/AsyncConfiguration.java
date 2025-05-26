8333424a87b94eb8f8179a9fb0f6379190cd1c4a
José Daniel Grayson <josedanielgrayson@proton.me>
2025-05-26 00:34:15 -0500

    refactor(config): remove unnecessary async configuration

--- src/main/java/com/turinmachin/unilife/configuration/AsyncConfiguration.java
+++ src/main/java/com/turinmachin/unilife/configuration/AsyncConfiguration.java
@@ -1,18 +1,9 @@
 package com.turinmachin.unilife.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@Configuration
 @EnableAsync
+@Configuration
 public class AsyncConfiguration {
-
-    @Bean(name = "taskExecutor")
-    public TaskExecutor taskExecutor() {
-        return new SimpleAsyncTaskExecutor();
-    }
-
 }
