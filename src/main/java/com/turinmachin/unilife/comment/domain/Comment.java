e6e2ff4b12b0e5ea049115c89ea3df3e1a254452
Martin59lilo <bonillasarmientomartin@gmail.com>
2025-05-27 12:03:38 -0500

    refactor(Comment): simplify methods equals and hashCode to readibility

--- src/main/java/com/turinmachin/unilife/comment/domain/Comment.java
+++ src/main/java/com/turinmachin/unilife/comment/domain/Comment.java
@@ -3,6 +3,7 @@ package com.turinmachin.unilife.comment.domain;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -59,28 +60,14 @@ public class Comment {
     private List<Comment> replies = new ArrayList<>();
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(id, comment.id);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Comment other = (Comment) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
-
 }
