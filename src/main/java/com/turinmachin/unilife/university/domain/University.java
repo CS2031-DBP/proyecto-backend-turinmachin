3e8b5f4d8d376919f23a7d7f1473514963ecc8c8
Martin59lilo <bonillasarmientomartin@gmail.com>
2025-05-27 12:07:27 -0500

    refactor(University): simplify methods equals and hashCode to readibility

--- src/main/java/com/turinmachin/unilife/university/domain/University.java
+++ src/main/java/com/turinmachin/unilife/university/domain/University.java
@@ -2,6 +2,7 @@ package com.turinmachin.unilife.university.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import com.turinmachin.unilife.degree.domain.Degree;
@@ -56,28 +57,14 @@ public class University {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        University that = (University) o;
+        return Objects.equals(id, that.id);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        University other = (University) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
-
 }
