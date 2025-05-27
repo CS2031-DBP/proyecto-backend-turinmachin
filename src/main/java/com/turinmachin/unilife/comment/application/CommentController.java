62f214a0a2146ddec044e66284606e4b19749815
José Daniel Grayson <josedanielgrayson@proton.me>
2025-05-26 21:20:36 -0500

    refactor: move part of PostController into new CommentController

---
+++ src/main/java/com/turinmachin/unilife/comment/application/CommentController.java
@@ -0,0 +1,134 @@
+package com.turinmachin.unilife.comment.application;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.turinmachin.unilife.comment.domain.Comment;
+import com.turinmachin.unilife.comment.domain.CommentService;
+import com.turinmachin.unilife.comment.dto.CommentResponseDto;
+import com.turinmachin.unilife.comment.dto.CreateCommentDto;
+import com.turinmachin.unilife.comment.dto.UpdateCommentDto;
+import com.turinmachin.unilife.comment.exception.CommentNotFoundException;
+import com.turinmachin.unilife.common.domain.ListMapper;
+import com.turinmachin.unilife.common.exception.ForbiddenException;
+import com.turinmachin.unilife.post.domain.Post;
+import com.turinmachin.unilife.post.domain.PostService;
+import com.turinmachin.unilife.post.exception.PostNotFoundException;
+import com.turinmachin.unilife.user.domain.Role;
+import com.turinmachin.unilife.user.domain.User;
+import com.turinmachin.unilife.user.domain.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/posts/{postId}/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ListMapper listMapper;
+
+    @GetMapping
+    public List<CommentResponseDto> getPostComments(@PathVariable UUID postId) {
+        Post post = postService.getPostById(postId).orElseThrow(PostNotFoundException::new);
+        return listMapper.map(post.getComments(), CommentResponseDto.class).toList();
+    }
+
+    @GetMapping("/{id}")
+    public CommentResponseDto getPostComments(@PathVariable UUID postId, @PathVariable UUID id) {
+        Comment comment = commentService.getPostCommentById(postId, id).orElseThrow(CommentNotFoundException::new);
+        return modelMapper.map(comment, CommentResponseDto.class);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public CommentResponseDto createPostComment(@PathVariable UUID postId, @Valid @RequestBody CreateCommentDto dto,
+            Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        userService.checkUserVerified(user);
+
+        Post post = postService.getPostById(postId).orElseThrow(PostNotFoundException::new);
+        Comment comment = commentService.createComment(dto, user, post);
+
+        return modelMapper.map(comment, CommentResponseDto.class);
+    }
+
+    @PostMapping("/{id}/replies")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public CommentResponseDto createPostCommentReply(@PathVariable UUID postId, @PathVariable UUID id,
+            @Valid @RequestBody CreateCommentDto dto,
+            Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        userService.checkUserVerified(user);
+
+        Comment parent = commentService.getPostCommentById(postId, id).orElseThrow(CommentNotFoundException::new);
+        Comment comment = commentService.createCommentReply(dto, user, parent);
+
+        return modelMapper.map(comment, CommentResponseDto.class);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public CommentResponseDto updatePostComment(@PathVariable UUID postId, @PathVariable UUID id,
+            @Valid @RequestBody UpdateCommentDto dto,
+            Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        userService.checkUserVerified(user);
+
+        Comment comment = commentService.getPostCommentById(postId, id).orElseThrow(CommentNotFoundException::new);
+
+        if (!user.equals(comment.getAuthor())) {
+            throw new ForbiddenException();
+        }
+
+        Comment updatedComment = commentService.updateComment(comment, dto);
+        return modelMapper.map(updatedComment, CommentResponseDto.class);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public void deletePostComment(@PathVariable UUID postId, @PathVariable UUID id, Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        userService.checkUserVerified(user);
+
+        Comment comment = commentService
+                .getPostCommentById(postId, id)
+                .orElseThrow(CommentNotFoundException::new);
+
+        if (user.getRole() == Role.USER && !user.equals(comment.getAuthor())) {
+            throw new ForbiddenException();
+        }
+
+        commentService.deleteComment(comment);
+    }
+
+}
--- src/main/java/com/turinmachin/unilife/post/application/PostController.java
+++ src/main/java/com/turinmachin/unilife/post/application/PostController.java
@@ -1,11 +1,27 @@
 package com.turinmachin.unilife.post.application;
 
-import com.turinmachin.unilife.comment.domain.Comment;
-import com.turinmachin.unilife.comment.domain.CommentService;
-import com.turinmachin.unilife.comment.dto.CommentResponseDto;
-import com.turinmachin.unilife.comment.dto.CreateCommentDto;
-import com.turinmachin.unilife.comment.dto.UpdateCommentDto;
-import com.turinmachin.unilife.comment.exception.CommentNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.turinmachin.unilife.common.domain.ListMapper;
 import com.turinmachin.unilife.common.exception.ForbiddenException;
 import com.turinmachin.unilife.common.exception.NotFoundException;
@@ -21,18 +37,8 @@ import com.turinmachin.unilife.post.infrastructure.PostSpecifications;
 import com.turinmachin.unilife.user.domain.Role;
 import com.turinmachin.unilife.user.domain.User;
 import com.turinmachin.unilife.user.domain.UserService;
-import jakarta.validation.Valid;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/posts")
@@ -43,9 +49,6 @@ public class PostController {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private CommentService commentService;
 
     @Autowired
     private ModelMapper modelMapper;
@@ -67,22 +70,13 @@ public class PostController {
         return listMapper.map(posts, PostResponseDto.class).toList();
     }
 
+    public void foo() {
+    }
+
     @GetMapping("/{id}")
     public PostResponseDto getPost(@PathVariable UUID id) {
         Post post = postService.getPostById(id).orElseThrow(PostNotFoundException::new);
         return modelMapper.map(post, PostResponseDto.class);
-    }
-
-    @GetMapping("/{id}/comments")
-    public List<CommentResponseDto> getPostComments(@PathVariable UUID id) {
-        Post post = postService.getPostById(id).orElseThrow(PostNotFoundException::new);
-        return listMapper.map(post.getComments(), CommentResponseDto.class).toList();
-    }
-
-    @GetMapping("/{id}/comments/{commentId}")
-    public CommentResponseDto getPostComments(@PathVariable UUID id, @PathVariable UUID commentId) {
-        Comment comment = commentService.getPostCommentById(id, commentId).orElseThrow(CommentNotFoundException::new);
-        return modelMapper.map(comment, CommentResponseDto.class);
     }
 
     @PostMapping
@@ -95,35 +89,6 @@ public class PostController {
 
         Post post = postService.createPost(dto, user);
         return modelMapper.map(post, PostResponseDto.class);
-    }
-
-    @PostMapping("/{id}/comments")
-    @ResponseStatus(HttpStatus.CREATED)
-    @PreAuthorize("hasRole('ROLE_USER')")
-    public CommentResponseDto createPostComment(@PathVariable UUID id, @Valid @RequestBody CreateCommentDto dto,
-            Authentication authentication) {
-        User user = (User) authentication.getPrincipal();
-        userService.checkUserVerified(user);
-
-        Post post = postService.getPostById(id).orElseThrow(PostNotFoundException::new);
-        Comment comment = commentService.createComment(dto, user, post);
-
-        return modelMapper.map(comment, CommentResponseDto.class);
-    }
-
-    @PostMapping("/{id}/comments/{commentId}/replies")
-    @ResponseStatus(HttpStatus.CREATED)
-    @PreAuthorize("hasRole('ROLE_USER')")
-    public CommentResponseDto createPostCommentReply(@PathVariable UUID id, @PathVariable UUID commentId,
-            @Valid @RequestBody CreateCommentDto dto,
-            Authentication authentication) {
-        User user = (User) authentication.getPrincipal();
-        userService.checkUserVerified(user);
-
-        Comment parent = commentService.getPostCommentById(id, commentId).orElseThrow(CommentNotFoundException::new);
-        Comment comment = commentService.createCommentReply(dto, user, parent);
-
-        return modelMapper.map(comment, CommentResponseDto.class);
     }
 
     @PutMapping("/{id}")
@@ -162,24 +127,6 @@ public class PostController {
         postService.setPostVote(post, user, VoteType.DOWNVOTE);
     }
 
-    @PutMapping("/{id}/comments/{commentId}")
-    @PreAuthorize("hasRole('ROLE_USER')")
-    public CommentResponseDto updatePostComment(@PathVariable UUID id, @PathVariable UUID commentId,
-            @Valid @RequestBody UpdateCommentDto dto,
-            Authentication authentication) {
-        User user = (User) authentication.getPrincipal();
-        userService.checkUserVerified(user);
-
-        Comment comment = commentService.getPostCommentById(id, commentId).orElseThrow(CommentNotFoundException::new);
-
-        if (!user.equals(comment.getAuthor())) {
-            throw new ForbiddenException();
-        }
-
-        Comment updatedComment = commentService.updateComment(comment, dto);
-        return modelMapper.map(updatedComment, CommentResponseDto.class);
-    }
-
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasRole('ROLE_USER')")
@@ -207,24 +154,6 @@ public class PostController {
                 .orElseThrow(() -> new NotFoundException("Post or vote not found"));
 
         postService.removePostVote(vote);
-    }
-
-    @DeleteMapping("/{id}/comments/{commentId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PreAuthorize("hasRole('ROLE_USER')")
-    public void deletePostComment(@PathVariable UUID id, @PathVariable UUID commentId, Authentication authentication) {
-        User user = (User) authentication.getPrincipal();
-        userService.checkUserVerified(user);
-
-        Comment comment = commentService
-                .getPostCommentById(id, commentId)
-                .orElseThrow(CommentNotFoundException::new);
-
-        if (user.getRole() == Role.USER && !user.equals(comment.getAuthor())) {
-            throw new ForbiddenException();
-        }
-
-        commentService.deleteComment(comment);
     }
 
 }
