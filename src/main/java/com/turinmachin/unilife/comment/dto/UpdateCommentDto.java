221414c68b942bc003fdc8989d3fcf088727950e
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-10 03:58:10 -0500

    refactor(comment): add size limit for comments

--- src/main/java/com/turinmachin/unilife/comment/dto/UpdateCommentDto.java
+++ src/main/java/com/turinmachin/unilife/comment/dto/UpdateCommentDto.java
@@ -1,11 +1,13 @@
 package com.turinmachin.unilife.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdateCommentDto {
 
     @NotBlank
+    @Size(max = 400)
     private String content;
 }
